f8c3d5ef6452eab6c02c46c3c3dc892bfd8ce8f9
SYTNIKMikhail-maker <sytnikmexail53@gmail.com>
2026-04-22 18:07:41 +0200

    Fixed mistakes and improved syntax
    Added Indentations  
    Created the fifth magical method 
    Fixed speed.setter
    Added necessery parent attributes 
    Moved from_string class method

--- OOP_first_task.py
+++ OOP_first_task.py
@@ -1,3 +1,4 @@
+
 from abc import ABC, abstractmethod
 
 class Transport(ABC):
@@ -6,23 +7,24 @@ class Transport(ABC):
         self.__speed = speed
         self.year = year
         self.price = price
+
     @abstractmethod
     def move(self):
         pass
+
     @abstractmethod
     def stop(self):
         pass
+
     @property
     def speed(self):
         return self.__speed
+
     @speed.setter
     def speed(self,value):
-        if self.__speed >= 0:
+        if value >= 0:
             self.__speed = value
-    @classmethod
-    def from_string(cls,data):
-        brand, speed, year, price = data.split(",")
-        return cls(brand,float(speed),year,float(price))
+
     @staticmethod
     def is_fast(value):
         return value > 200
@@ -34,56 +36,83 @@ class Transport(ABC):
         return len(self.brand)
     def __lt__(self,other):
         return self.price < other.price
+
+
 class Engine:
     def __init__(self,engine_type,horsepower):
         self.engine_type =engine_type
         self.horsepower = horsepower
+
+    @classmethod
+    def from_string(cls, data):
+        engine_type, horsepower = data.split(",")
+        return cls(engine_type, float(horsepower))
+
     def start_engine(self):
         print(f"Engine {self} is running")
+
     def __add__(self,other):
         return self.horsepower + other.horsepower
+
+    def __sub__(self, other):
+        return self.horsepower - other.horsepower
+
+
 class Car(Transport, Engine):
     def __init__(self,brand,speed,year,price,engine_type,horsepower):
-     super().__init__(brand,speed,year,price)
-     self.engine_type = engine_type
-     self.horsepower = horsepower
+        Transport.__init__(self,brand,speed,year,price)
+        Engine.__init__(self,engine_type,horsepower)
+
     def move(self):
         print(f"{self}  has gone")
+
     def stop(self):
         print(f"{self}  has stopped")
+
+
 class Bike(Transport):
     def __init__(self,brand,speed,year,price,horsepower,wheels):
-        super().__init__(brand,speed,year,price)
+        Transport.__init__(self, brand, speed, year, price)
         self.horsepower = horsepower
         self.wheels = wheels
+
     def move(self):
         print(f"{self} bike has gone")
+
     def stop(self):
         print(f"{self} bike has stopped")
+
+
 class Plane(Transport,Engine):
     def __init__(self,brand,speed,year,price,engine_type,horsepower,size):
-        super().__init__(brand,speed,year,price)
-        self.engine_type = engine_type
-        self.horsepower = horsepower
+        Transport.__init__(self, brand, speed, year, price)
+        Engine.__init__(self, engine_type, horsepower)
         self.size = size
+
     def move(self):
         print(f"{self} took off")
+
     def stop(self):
         print(f"{self} has landed")
+
+
 class Ship(Transport, Engine):
-    def __init__(self,brand,speed,year,price,engine_type,able_distance):
-        super().__init__(brand, speed, year, price)
-        self.engine_type = engine_type
+    def __init__(self,brand,speed,year,price,engine_type,horsepower,able_distance):
+        Transport.__init__(self, brand, speed, year, price)
+        Engine.__init__(self, engine_type, horsepower)
         self.able_distance = able_distance
+
     def move(self):
         print(f"{self} ship sailed away")
+        
     def stop(self):
         print(f"{self} has swum up")
 
 
-car1 = Car("Toyota", 150, 2020, 15000, "petrol", 120)
-car2 = Car("BMW", 250, 2022, 30000, "petrol", 200)
+car1 = Car("Toyota", 150, 2020, 15000, "бензин", 120)
+car2 = Car("BMW", 250, 2022, 30000, "бензин", 200)
 print(car1)
 print(car1 == car2)
 print(car1 < car2)
-print(car1 + car2
+print(car1 + car2)
+print(car1 - car2)
