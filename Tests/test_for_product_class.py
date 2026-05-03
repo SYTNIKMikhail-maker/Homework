89780d2fd0c2e52151e3e3d971c15c85a6a930f0
mikhail <sytnikmexail53@email.com>
2026-05-03 13:35:44 +0200

    Created a new branch and installed tasks

---
+++ Tests/test_for_product_class.py
@@ -0,0 +1,22 @@
+
+
+import pytest
+from testing_to_test import Product
+
+
+@pytest.fixture
+def product():
+    return Product("Apple",1.5,100)
+
+def test_subtract_quantity(product):
+    product.subtract_quantity(10)
+    assert product.quantity == 90
+
+def test_add_quantity(product):
+    product.add_quantity(50)
+    assert product.quantity == 150
+
+def test_change_price(product):
+    product.change_price(2.0)
+    assert product.price == 2.0
+
